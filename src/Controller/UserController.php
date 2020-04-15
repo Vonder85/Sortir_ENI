969066c3285269ad4f3989e172279f290d90fd00
jen <jenny-di@hotmail.fr>
2020-04-15 10:37:33 +0200

    Modif UserController photo

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -28,8 +28,8 @@ class UserController extends AbstractController
             $hashed = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hashed);
 
-            $photo = $registerForm['photo']->getData();
-            $photoName = $this->generateUniqueFileName().'.'.$photo->guessExtension();
+            $photo = $registerForm->get('photo')->getData();
+            $photoName = $this->generateUniqueFileName().'.'.strtolower($photo->getClientOriginalExtension());
             $photo->move(
                 $this->getParameter('upload_photos'),
                 $photoName
